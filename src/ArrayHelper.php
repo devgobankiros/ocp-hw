de36bac911f5421dade4fdad8fc81f32153cacac
auto-pull <mail@bankiros.ru>
2024-02-14 22:08:53 +0300

    feat(ocp-hw): hw3
    added array helper class
    
    main

---
+++ src/ArrayHelper.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Devgobankiros\OcpHw;
+
+class ArrayHelper
+{
+    public function getArrayCount(array $data): int
+    {
+        return count($data);
+    }
+}
